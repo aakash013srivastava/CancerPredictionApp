02f854e96a20541814574f8e7bc0b46999e62a23
aakash013srivastava <aakash013srivastava@gmail.com>
2023-05-31 20:54:16 +0530

    Column addition/removal fields added to gui

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ class Cancer:
     def __init__(self):
         self.root = Tk()
         self.root.title = "Used Car Price Prediction App"
-        self.root.geometry('600x600')
+        self.root.geometry('1000x600')
         
         self.lbl_fopen = ttk.Label(self.root,text="Open dataset:")
         self.lbl_fopen.grid(row=1,column=1)
         self.button_file_entry = ttk.Button(self.root,text="Select File",command=self.openFile)
         self.button_file_entry.grid(row=1,column=3)
 
-        self.columns = [x for x in self.df.columns] 
+        
 
         if __name__ == "__main__":
             self.root.mainloop()
@@ -32,15 +32,44 @@ class Cancer:
             print(self.df.columns,end="\n")
         except Exception as e:
             print(e)
-        
-        self.generate_input_data_fields()
 
-    
-    def generate_input_data_fields(self):
-        pass
-        # self.labels
+        self.columns = [x for x in self.df.columns] 
+        # print(self.columns)
+
+        self.lbl_dependent = ttk.Label(self.root,text="Select Dependent Feature:")
+        self.lbl_dependent.grid(row=2,column=1)
+
+        self.combo_dependent = ttk.Combobox(self.root,values=self.columns)
+        self.combo_dependent.grid(row=2,column=3)
+
+        self.btn_combo_selected = ttk.Button(text="Select",command=self.delete_fields)
+        self.btn_combo_selected.grid(row=2,column=5)
+
+        
+    def delete_fields(self):
+        self.fields = ""
+        for num,name in enumerate(self.df.columns):
+            self.fields+= str(num)+":="+name+"||"
+        self.lbl_disp_fields = ttk.Label(self.root,text=self.fields)
+        self.lbl_disp_fields.grid(row=3,column=1)
+        self.lbl_ask_del_fields = ttk.Label(self.root,text="Enter comma-separated field numbers to delete")
+        self.lbl_ask_del_fields.grid(row=4,column=1)
+        self.del_entry = ttk.Entry(self.root)    
+        self.del_entry.grid(row=4,column=3)
+        self.btn_del_activate = ttk.Button(self.root,text="Delete",command=self.log_reg)
+        self.btn_del_activate.grid(row=4,column=5)
 
     def log_reg(self):
+        col_nums = self.del_entry.get().split(',')
+        self.delete_list = [self.df.columns[int(x)] for x in col_nums]
+        self.delete_list.append(self.combo_dependent.get())
+        print(self.delete_list)
+        
+        
+        self.after_delete_cols = [x for x in self.df.columns if x not in self.delete_list]
+        print(self.after_delete_cols)
+        self.df = self.df.drop(self.df[self.after_delete_cols],axis=1)
+        
         
         X = self.df.iloc[:,2:].values
         y = self.df.iloc[:,1].values
